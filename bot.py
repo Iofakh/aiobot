34406fe19d9546f7c25e62b570a0c2c148e78016
Александр Храмов <it.khramov@gmail.com>
2022-12-23 17:26:11 +0300

    Swap

---
+++ bot.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+# импорты
+from config_reader import config
+
+# Для записей с типом Secret* необходимо 
+# вызывать метод get_secret_value(), 
+# чтобы получить настоящее содержимое вместо '*******'
+bot = Bot(token=config.bot_token.get_secret_value())
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+
+
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
